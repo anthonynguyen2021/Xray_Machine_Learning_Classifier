d8ec994ccd194c2ad40595e5b5356cde41c93277
Anthony <antngu@ucdavis.edu>
2022-12-22 13:00:10 -0800

    clean up code and using best practices

--- conversion.py
+++ conversion.py
@@ -1,7 +1,7 @@
 '''
 Python Script written by Anthony Nguyen
 
-Usage: Make sure to comment function not used in {convert_image,list_unique_x_ray}
+Usage: Make sure to comment function not used in {convert_image, list_unique_x_ray}
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------    
     
@@ -85,9 +85,14 @@ from PIL import ImageOps
 import random
 
 # Function that resize all images in source_path to dimension num_rows by num_cols and saves them in destination_path 
-def convert_image(source_path,destination_path,num_rows,num_cols,resize,horizontal_reflection,rotate_image,degree,gauss_noise,mean,std_dev,shear_horizontal,horizontal_factor,shear_vertical,vertical_factor,zero_padding,padding_dim,aspect,dimension,label_dict,print_every=100):
+def convert_image(
+        source_path, destination_path, num_rows, num_cols, resize, horizontal_reflection,
+        rotate_image, degree, gauss_noise, mean, std_dev, shear_horizontal, horizontal_factor,
+        shear_vertical, vertical_factor, zero_padding, padding_dim, aspect, dimension, label_dict,
+        print_every=100
+    ):
 
-    path = [source_path,destination_path]
+    path = [source_path, destination_path]
     
     # Get all the file names in the folder
     list_files = os.listdir(path[0])
@@ -101,19 +106,22 @@ def convert_image(source_path,destination_path,num_rows,num_cols,resize,horizont
     
     # Iterate over all the images, perform averaging over the RGB channels, rescale to N x N, and save image in destination    
     for file in list_files:
+
         # Print files processed every 100 x-rays
         if files_processed % print_every == 0:
             print(str(files_processed) + ' files processed out of ' + str(num_files) + ' files.')
         files_processed += 1
+
         # Take an image in name folder and open it
         image = Image.open(path[0] + '\\' + file)
+
         # Try averaging over colored image
         try:
             # Averaging images over RGB channels
             image_array = np.array(image)
-            image_average = np.zeros((np.shape(image_array)[0],np.shape(image_array)[1]))
+            image_average = np.zeros((np.shape(image_array)[0], np.shape(image_array)[1]))
             for i in range(3):
-                image_average += image_array[:,:,i]
+                image_average += image_array[:, :, i]
             image_average /= 3
                 
             # Preprocessing so every image is in the format 0-255 by np.uint8
@@ -122,6 +130,7 @@ def convert_image(source_path,destination_path,num_rows,num_cols,resize,horizont
         # Gray scale images    
         except IndexError: 
             pass
+
         # Aspect ratio rescaling
         if aspect == True:
             List = image.size
@@ -129,54 +138,63 @@ def convert_image(source_path,destination_path,num_rows,num_cols,resize,horizont
             index_min = List.index(min(List))
             aspect_ratio = List[index_max] / List[index_min]
             if index_max == 1:
-                image = image.resize((int(dimension/ aspect_ratio),dimension))
+                image = image.resize((int(dimension / aspect_ratio), dimension))
             else:
                 image = image.resize((dimension, int(dimension / aspect_ratio)))
+
         # Reflection
         if horizontal_reflection == True:
             image = ImageOps.mirror(image)
+
         # Rotation
         if rotate_image == True:
             image = image.rotate(degree)
+
         # Resizing
         # Then resize the image to num_rows x num_cols
         if resize == True:
-            image = image.resize((num_rows,num_cols))
+            image = image.resize((num_rows, num_cols))
+
         # Gaussian Noise
         if gauss_noise == True:
             img = np.array(image)  
             # add Gaussian Noise
-            noisy_img = img + np.random.normal(mean,std_dev,img.shape)
+            noisy_img = img + np.random.normal(mean, std_dev, img.shape)
             # remove out of range pixel values
-            noisy_img_clipped = np.clip(noisy_img,0,255)
+            noisy_img_clipped = np.clip(noisy_img, 0, 255)
             # reformat to get image to render
             noisy_img_clipped_format = noisy_img_clipped.astype(np.uint8)
             image = Image.fromarray(noisy_img_clipped_format)
+
         # Shearing Images
         if shear_horizontal == True:
-            image = image.transform(image.size,Image.AFFINE,(1,horizontal_factor,0,0,1,0))
+            image = image.transform(image.size, Image.AFFINE, (1, horizontal_factor, 0, 0, 1, 0))
         if shear_vertical == True:
-            image = image.transform(image.size,Image.AFFINE,(1,0,0,vertical_factor,1,0))
+            image = image.transform(image.size, Image.AFFINE, (1, 0, 0, vertical_factor, 1, 0))
         if zero_padding == True:
             if np.shape(image)[0] < padding_dim and np.shape(image)[1] < padding_dim:
-                zero_matrix = np.zeros((padding_dim,padding_dim))
+                zero_matrix = np.zeros((padding_dim, padding_dim))
                 image_matrix = np.array(image)
-                zero_matrix[((padding_dim-image_matrix.shape[0])//2):image_matrix.shape[0]+(((padding_dim-image_matrix.shape[0])//2)),(((padding_dim-image_matrix.shape[1])//2)):image_matrix.shape[1]+(((padding_dim-image_matrix.shape[1])//2))] = image_matrix
+                zero_matrix[((padding_dim - image_matrix.shape[0]) // 2):image_matrix.shape[0]+(((padding_dim - image_matrix.shape[0]) // 2)), (((padding_dim - image_matrix.shape[1]) // 2)):image_matrix.shape[1]+(((padding_dim - image_matrix.shape[1]) // 2))] = image_matrix
                 zero_matrix_format = zero_matrix.astype(np.uint8)
                 image = Image.fromarray(zero_matrix_format)
             else:
                 print('no padding for image' + file)
+
         try:
             label_dict[file][0] # first element is label, if x-ray not in metadata.csv, go to except
             # Save image in destination folder in format .jpg
             image.save(path[1] + '\\' + file,'JPEG')
         except: # label not found -- skip and increment
             bad_labels.append(file)
+
     if resize == True:
         print('\nYour images are resized at ' + str(num_rows) + ' by ' + str(num_cols) + ' and are in the path ' + destination_path + '\n')
+
     print(f'--- There were {len(bad_labels)} images without labels ---')
-    
-def list_unique_x_ray(source_path,destination_path,rng,viral,normal_rng):
+
+
+def list_unique_x_ray(source_path, destination_path, rng, viral, normal_rng):
 
     list_files = os.listdir(source_path)
     
@@ -187,22 +205,24 @@ def list_unique_x_ray(source_path,destination_path,rng,viral,normal_rng):
     for file in list_files:
         if ('person' in file) or ('IM' in file) or ('NORMAL' in file):
             list_files_person.append(file)
-            
-    
+
     person_string = 'person'
     index = len('person')    
     
     # Append 1 x-ray per patient in list_unique_person_files
     list_unique_person_files = []
     person_x_rays = []
+
     for file in list_files_person:
         
         # Get Patient ID 
         stack = person_string
         j = index
+
         while file[j] != '_':
             stack += file[j]
             j += 1
+
         # Append x-ray corresponding to patient
         if not person_x_rays:
             person_x_rays.append(file)
@@ -212,11 +232,14 @@ def list_unique_x_ray(source_path,destination_path,rng,viral,normal_rng):
             if stack in person_x_rays[-1]:
                 person_x_rays.append(file)
             else:
+
                 if rng:
                     # Randomly pick an xray from a person
-                    random_index = random.randint(0,len(person_x_rays)-1)
+                    random_index = random.randint(0, len(person_x_rays) - 1)
                     list_unique_person_files.append(person_x_rays[random_index])
+
                 if viral:
+
                     # Append the first virus x-ray coresponding to a patient. If not found, take a bacterial x-ray.
                     virus_count = 0
                     for item in person_x_rays:                       
@@ -224,8 +247,10 @@ def list_unique_x_ray(source_path,destination_path,rng,viral,normal_rng):
                             virus_count += 1
                             list_unique_person_files.append(item)
                             break
+
                     if virus_count == 0:
-                        list_unique_person_files.append(person_x_rays[0])                    
+                        list_unique_person_files.append(person_x_rays[0])
+                
                 # clear list to load x-rays for the next person
                 person_x_rays = []
                 # append file in person_x_rays
@@ -255,26 +280,29 @@ def list_unique_x_ray(source_path,destination_path,rng,viral,normal_rng):
             
             # If x-ray has string 'NORMAL', push it in the stack if it corresponds to the other x-rays in the stack or clear stack and push x-ray in stack
             if 'NORMAL2' in file:
+
                 file_name = file.split('.')
                 file_name.pop()
                 file_name = '-'.join(file_name)
                 file_name = file_name.split('-')
                 file_name.pop(0)
                 file_name.pop(0)
+
                 if len(file_name) == 3:
                     file_name.pop()
                 file_name = '-'.join(file_name)
+
                 if file_name in stack_normal_patient[0]:
                     stack_normal_patient.append(file)
                 else:
                     # If normal_rng == True, randomly pick an x-ray in stack_normal_patient and append to list_unique_person_files
                     if normal_rng:
-                        list_unique_person_files.append(stack_normal_patient[random.randint(0,len(stack_normal_patient)-1)])
+                        list_unique_person_files.append(stack_normal_patient[random.randint(0, len(stack_normal_patient) - 1)])
                     else:
                         list_unique_person_files.append(stack_normal_patient[0])
+
                     stack_normal_patient = []
                     stack_normal_patient.append(file)
-            
             # Push in stack if x-ray corresponds to other x-rays in the stack or clear stack and push in stack
             else:
                 file_name = file.split('.')
@@ -283,25 +311,25 @@ def list_unique_x_ray(source_path,destination_path,rng,viral,normal_rng):
                 file_name = file_name.split('-')
                 #file_name.pop(0)
                 file_name.pop(0)
+
                 if len(file_name) == 3:
                     file_name.pop()
+
                 file_name = '-'.join(file_name)
                 if file_name in stack_normal_patient[0]:
                     stack_normal_patient.append(file)
                 else:
                     # If normal_rng == True, randomly pick an x-ray in stack_normal_patient and append to list_unique_person_files
                     if normal_rng:
-                        list_unique_person_files.append(stack_normal_patient[random.randint(0,len(stack_normal_patient)-1)])
+                        list_unique_person_files.append(stack_normal_patient[random.randint(0, len(stack_normal_patient) - 1)])
                     else:
                         list_unique_person_files.append(stack_normal_patient[0])
+
                     stack_normal_patient = []
                     stack_normal_patient.append(file)
-                
-                   
-    
+
     # Number of files in source path
     num_files = len(list_unique_person_files)   
-    
     
     # Counter for files processed
     files_processed = 1    
@@ -309,18 +337,21 @@ def list_unique_x_ray(source_path,destination_path,rng,viral,normal_rng):
     # Iterate over all the images, perform averaging over the RGB channels, rescale to N x N, and save image in destination 
     print("--- Processing of unique {} files ---".format(num_files))
     for file in list_unique_person_files:
+
         if files_processed % print_every == 0:
             print(str(files_processed) + ' files processed out of ' + str(num_files) + ' files.')
         files_processed += 1
+
         # Take an image in name folder and open it
         image = Image.open(path[0] + os.path.sep + file)
+
         # Try averaging over colored image
         try:
             # Averaging images over RGB channels
             image_array = np.array(image)
-            image_average = np.zeros((np.shape(image_array)[0],np.shape(image_array)[1]))
+            image_average = np.zeros((np.shape(image_array)[0], np.shape(image_array)[1]))
             for i in range(3):
-                image_average += image_array[:,:,i]
+                image_average += image_array[:, :, i]
             image_average /= 3
                 
             # Preprocessing so every image is in the format 0-255 by np.uint8
@@ -329,8 +360,10 @@ def list_unique_x_ray(source_path,destination_path,rng,viral,normal_rng):
         # Gray scale images    
         except IndexError: 
             pass
+
         image.save(path[1] + os.path.sep + file,'JPEG')
-     
+
+
 def get_target_paths(base_path):
     train_path = f'{base_path}\\train'
     train_dest = f'{base_path}\\train_modified'
@@ -340,8 +373,10 @@ def get_target_paths(base_path):
 
         
 def check_target_paths(target_paths, label_names):
+
     for path in target_paths:
         for label in label_names:
+
             try:
                 os.stat(f'{path}\\{label}')
                 print(f"-> Verified target path: {path}\\{label}")
@@ -350,7 +385,7 @@ def check_target_paths(target_paths, label_names):
                 os.mkdir(f'{path}\\{label}')
         
         
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     
     # Specify paths
         
@@ -360,8 +395,11 @@ if (__name__ == "__main__"):
     test_destination_path = 'C:\\Users\\Tony Nguyen\\Desktop\\Coronahack_STAT208\\coronahack-chest-xraydataset\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset\\test_modified'
     
     # Run the image conversion
-    convert_image(train_path,train_destination_path,1000,500,False,False,False,10,False,50,1,False,0.2,False,0.2,True,600,True,595,label_dict,print_every=500)
+    convert_image(
+        train_path, train_destination_path, 1000, 500, False, False,
+        False, 10, False, 50, 1, False, 0.2, False, 0.2, True, 600,
+        True, 595, label_dict, print_every=500
+        )
     
     # Extract Unique x-rays from source_path to destination_path
-    list_unique_x_ray(test_path,test_destination_path,True,False,True)
-
+    list_unique_x_ray(test_path, test_destination_path, True, False, True)
